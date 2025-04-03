1fdb66b87c727a007e404d8127a47bea45dcf447
Peter Polidoro <peter@polidoro.io>
2025-04-03 10:21:53 -0400

    Remove serial commands

--- hex_maze_interface/cli.py
+++ hex_maze_interface/cli.py
@@ -12,11 +12,6 @@ def cli(ctx):
 
 @cli.command()
 @click.pass_obj
-def say_hello(hmi):
-    hmi.say_hello()
-
-@cli.command()
-@click.pass_obj
 def discover_cluster_ip_addresses(hmi):
     cluster_ip_addresses = hmi.discover_cluster_ip_addresses()
     print(cluster_ip_addresses)
@@ -29,40 +24,16 @@ def get_cluster_address_map(hmi):
 
 @cli.command()
 @click.pass_obj
-def find_serial_ports(hmi):
-    print(hmi.find_serial_ports())
+def reset(hmi):
+    hmi.reset()
 
 @cli.command()
 @click.pass_obj
-def connect_serial(hmi):
-    print(hmi.connect_serial())
+def power_off(hmi):
+    hmi.power_off()
 
 @cli.command()
 @click.pass_obj
-def led_on_str(hmi):
-    hmi.led_on_str()
+def power_on(hmi):
+    hmi.power_on()
 
-@cli.command()
-@click.pass_obj
-def led_off_str(hmi):
-    hmi.led_off_str()
-
-# interface = HexMazeInterface()
-
-# CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
-
-# @click.command(context_settings=CONTEXT_SETTINGS)
-# @click.option('--ip', help='IP address to connect to')
-# def cli(ip):
-#     """Command line interface to the Voigts lab hex maze."""
-#     clear_screen()
-#     click.echo(f"Connecting to {ip}")
-#     interface.connect(ip)
-#     interface.send_led_on()
-
-# def clear_screen():
-#     """Clear command line for various operating systems."""
-#     if (os.name == 'posix'):
-#         os.system('clear')
-#     else:
-#         os.system('cls')
--- hex_maze_interface/hex_maze_interface.py
+++ hex_maze_interface/hex_maze_interface.py
@@ -1,13 +1,11 @@
 """Python interface to the Reiser lab ArenaController."""
 import socket
-import atexit
 import nmap3
-from serial_interface import SerialInterface, find_serial_interface_ports
 
 
 PORT = 7777
 IP_RANGE = '192.168.10.0/24'
-SERIAL_BAUDRATE = 115200
+REPEAT_LIMIT = 4
 
 def results_filter(pair):
     key, value = pair
@@ -31,33 +29,41 @@ class HexMazeInterface():
         self._cluster_ip_addresses = None
         self._nmap = nmap3.NmapHostDiscovery()
         self._socket = None
-        self._serial_interface = None
-        atexit.register(self._exit)
-
-    def _exit(self):
-        try:
-            self._socket.close()
-        except AttributeError:
-            pass
-        try:
-            self._serial_interface.close()
-        except AttributeError:
-            pass
 
     def _debug_print(self, *args):
         """Print if debug is True."""
         if self._debug:
             print(*args)
 
-    def _send(self, msg):
+    def _send_and_receive(self, msg):
         """Send message and receive response."""
-        if self._socket:
-            totalsent = 0
-            while totalsent < len(msg):
-                sent = self._socket.send(msg[totalsent:])
-                if sent == 0:
-                    raise RuntimeError("socket connection broken")
-                totalsent = totalsent + sent
+        repeat_count = 0
+        while repeat_count < REPEAT_LIMIT:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                self._debug_print(f'to {IP_ADDRESS} port {PORT}')
+                s.settimeout(2)
+                try:
+                    s.connect((IP_ADDRESS, PORT))
+                    s.sendall(msg)
+                    response = s.recv(1024)
+                    break
+                except (TimeoutError, OSError):
+                    self._debug_print('socket timed out')
+                    response = None
+                    repeat_count += 1
+        self._debug_print('response: ', response)
+
+    def reset(self):
+        """Reset cluster microcontroller."""
+        self._send_and_receive(b'\x01')
+
+    def power_off(self):
+        """Turn all prisms on."""
+        self._send_and_receive(b'\x02')
+
+    def all_on(self):
+        """Turn all prisms on."""
+        self._send_and_receive(b'\x03')
 
     def discover_cluster_ip_addresses(self):
         results = self._nmap.nmap_portscan_only(IP_RANGE, args=f'-p {PORT}')
@@ -65,49 +71,18 @@ class HexMazeInterface():
         self._cluster_ip_addresses = list(filtered_results.keys())
         return self._cluster_ip_addresses
 
-    def find_serial_ports(self):
-        return find_serial_interface_ports()
-
-    def connect_serial(self):
-        self.si = SerialInterface()
-
     def get_cluster_address_map(self):
         if self._cluster_ip_addresses is None:
             self.discover_cluster_ip_addresses()
 
         self._cluster_address_map = {}
-        for cluster_ip_address in self._cluster_ip_addresses:
-            _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            _socket.connect((cluster_ip_address, PORT))
-            _socket.sendall(b'GET_ADDRESSES\n')
-            data = _socket.recv(1024)
-            decoded_data = data.decode('utf-8')
-            split_data = decoded_data.split(' ')
-            self._cluster_address_map[int(split_data[0])] = {'ip_address': split_data[1]}
-            _socket.close()
+        # for cluster_ip_address in self._cluster_ip_addresses:
+        #     _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #     _socket.connect((cluster_ip_address, PORT))
+        #     _socket.sendall(b'GET_ADDRESSES\n')
+        #     data = _socket.recv(1024)
+        #     decoded_data = data.decode('utf-8')
+        #     split_data = decoded_data.split(' ')
+        #     self._cluster_address_map[int(split_data[0])] = {'ip_address': split_data[1]}
+        #     _socket.close()
         return self._cluster_address_map
-
-    def disconnect(self, ip_address):
-        """Shutdown and close socket connect at ip address."""
-        self._debug_print('HexMazeInterface disconnecting connecting...')
-        self._socket.shutdown()
-        self._socket.close()
-        self._debug_print('HexMazeInterface connected')
-
-    def send_hello_world(self):
-        """Send test message."""
-        self._send(b"Hello, World!")
-
-    def led_on(self):
-        """Send LED_ON message."""
-        message = "LED_ON"
-        self._socket.sendall(message.encode())
-
-    def led_on_str(self):
-        """Send LED_ON message."""
-        message = "LED_ON"
-        self._socket.sendall(message.encode())
-
-    def say_hello(self):
-        print("hello!")
-
