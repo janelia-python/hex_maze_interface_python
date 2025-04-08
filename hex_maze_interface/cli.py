e3d89570f0b6eb39f15a7cf5fb3d1a8640b2f1b0
Peter Polidoro <peter@polidoro.io>
2025-04-08 15:53:40 -0400

    Update commands

--- hex_maze_interface/cli.py
+++ hex_maze_interface/cli.py
@@ -13,27 +13,67 @@ def cli(ctx):
 @cli.command()
 @click.pass_obj
 def discover(hmi):
-    cluster_ip_addresses = hmi.discover_cluster_ip_addresses()
-    print(cluster_ip_addresses)
+    ip_addresses = hmi.discover_ip_addresses()
+    print(ip_addresses)
 
 @cli.command()
 @click.pass_obj
-def get_cluster_address_map(hmi):
-    cluster_address_map = hmi.get_cluster_address_map()
+def map(hmi):
+    cluster_address_map = hmi.map_cluster_addresses()
     print(cluster_address_map)
 
 @cli.command()
+@click.argument('cluster-address', nargs=1, type=int)
 @click.pass_obj
-def reset(hmi):
-    hmi.reset()
+def check(hmi, cluster_address):
+    if hmi.check_communication(cluster_address):
+        print('communicating')
+    else:
+        print('not communicating!')
 
 @cli.command()
+@click.argument('cluster-address', nargs=1, type=int)
 @click.pass_obj
-def power_off(hmi):
-    hmi.power_off()
+def no_cmd(hmi, cluster_address):
+    if hmi.no_cmd(cluster_address):
+        print('no command received proper error response')
+    else:
+        print('no command did not receive proper error response!')
 
 @cli.command()
+@click.argument('cluster-address', nargs=1, type=int)
 @click.pass_obj
-def power_on(hmi):
-    hmi.power_on()
+def bad_cmd(hmi, cluster_address):
+    if hmi.bad_cmd(cluster_address):
+        print('bad command received proper error response')
+    else:
+        print('bad command did not receive proper error response!')
+
+@cli.command()
+@click.argument('cluster-address', nargs=1, type=int)
+@click.pass_obj
+def reset(hmi, cluster_address):
+    if hmi.reset(cluster_address):
+        print('resetting')
+    else:
+        print('not resetting')
+
+@cli.command()
+@click.argument('cluster-address', nargs=1, type=int)
+@click.argument('duration-ms', nargs=1, type=int)
+@click.pass_obj
+def beep(hmi, cluster_address, duration_ms):
+    hmi.beep(cluster_address, duration_ms)
+
+@cli.command()
+@click.argument('cluster-address', nargs=1, type=int)
+@click.pass_obj
+def power_off(hmi, cluster_address):
+    hmi.power_off(cluster_address)
+
+@cli.command()
+@click.argument('cluster-address', nargs=1, type=int)
+@click.pass_obj
+def power_on(hmi, cluster_address):
+    hmi.power_on(cluster_address)
 
--- hex_maze_interface/hex_maze_interface.py
+++ hex_maze_interface/hex_maze_interface.py
@@ -1,11 +1,14 @@
 """Python interface to the Reiser lab ArenaController."""
 import socket
 import nmap3
+import struct
 
 
 PORT = 7777
-IP_RANGE = '192.168.10.0/24'
+IP_BASE = '192.168.10.'
+IP_RANGE = IP_BASE + '0/24'
 REPEAT_LIMIT = 4
+PROTOCOL_VERSION = b'\x01'
 
 def results_filter(pair):
     key, value = pair
@@ -25,7 +28,7 @@ class HexMazeInterface():
         """Initialize a HexMazeInterface instance."""
         self._debug = debug
         self._clusters = None
-        self._cluster_ip_addresses = None
+        self._cluster_address_map = {}
         self._nmap = nmap3.NmapHostDiscovery()
         self._socket = None
 
@@ -34,54 +37,84 @@ class HexMazeInterface():
         if self._debug:
             print(*args)
 
-    def _send_and_receive(self, msg):
-        """Send message and receive response."""
+    def _send_ip_msg_receive_rsp(self, ip_address, msg):
+        """Send message to IP address and receive response."""
         repeat_count = 0
+        rsp = None
+        self._debug_print('msg: ', msg.hex())
         while repeat_count < REPEAT_LIMIT:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                self._debug_print(f'to {IP_ADDRESS} port {PORT}')
+                self._debug_print(f'to {ip_address} port {PORT}')
                 s.settimeout(2)
                 try:
-                    s.connect((IP_ADDRESS, PORT))
+                    s.connect((ip_address, PORT))
                     s.sendall(msg)
-                    response = s.recv(1024)
+                    rsp = s.recv(1024)
                     break
                 except (TimeoutError, OSError):
                     self._debug_print('socket timed out')
-                    response = None
                     repeat_count += 1
-        self._debug_print('response: ', response)
+        self._debug_print('rsp: ', rsp.hex())
+        return rsp
 
-    def reset(self):
+    def _send_ip_cmd_receive_rsp(self, ip_address, cmd):
+        msg = PROTOCOL_VERSION + cmd
+        return self._send_ip_msg_receive_rsp(ip_address, msg)
+
+    def _send_cluster_cmd_receive_rsp(self, cluster_address, cmd):
+        ip_address = IP_BASE + str(cluster_address)
+        return self._send_ip_cmd_receive_rsp(ip_address, cmd)
+
+    def discover_ip_addresses(self):
+        results = self._nmap.nmap_portscan_only(IP_RANGE, args=f'-p {PORT}')
+        filtered_results = dict(filter(results_filter, results.items()))
+        return list(filtered_results.keys())
+
+    def map_cluster_addresses(self):
+        ip_addresses = self.discover_ip_addresses()
+        self._cluster_address_map = {}
+        for ip_address in ip_addresses:
+            cluster_address = self.read_cluster_address(ip_address)
+            self._cluster_address_map[cluster_address] = ip_address
+        return self._cluster_address_map
+
+    def read_cluster_address(self, ip_address):
+        cmd = b'\x01'
+        rsp = self._send_ip_cmd_receive_rsp(ip_address, cmd)
+        return int(rsp[0])
+
+    def check_communication(self, cluster_address):
+        """Check communication with cluster."""
+        cmd = b'\x02'
+        rsp = self._send_cluster_cmd_receive_rsp(cluster_address, cmd)
+        return rsp == b'\x12\x34\x56\x78'
+
+    def no_cmd(self, cluster_address):
+        """Send no command to get error response."""
+        cmd = b''
+        rsp = self._send_cluster_cmd_receive_rsp(cluster_address, cmd)
+        return rsp == b'\xEE'
+
+    def bad_cmd(self, cluster_address):
+        """Send bad command to get error response."""
+        cmd = b'\xEE'
+        rsp = self._send_cluster_cmd_receive_rsp(cluster_address, cmd)
+        return rsp == b'\xEE'
+
+    def reset(self, cluster_address):
         """Reset cluster microcontroller."""
-        self._send_and_receive(b'\x01')
+        cmd = b'\x03'
+        rsp = self._send_cluster_cmd_receive_rsp(cluster_address, cmd)
+        return rsp == cmd
+
+    def beep(self, cluster_address, duration_ms):
+        """Command cluster to beep for duration."""
+        cmd = struct.pack('<BH', 0x04, duration_ms)
+        rsp = self._send_cluster_cmd_receive_rsp(cluster_address, cmd)
+        return rsp == cmd
 
     def power_off(self):
         """Turn all prisms on."""
-        self._send_and_receive(b'\x02')
 
-    def all_on(self):
+    def power_on(self):
         """Turn all prisms on."""
-        self._send_and_receive(b'\x03')
-
-    def discover_cluster_ip_addresses(self):
-        results = self._nmap.nmap_portscan_only(IP_RANGE, args=f'-p {PORT}')
-        filtered_results = dict(filter(results_filter, results.items()))
-        self._cluster_ip_addresses = list(filtered_results.keys())
-        return self._cluster_ip_addresses
-
-    def get_cluster_address_map(self):
-        if self._cluster_ip_addresses is None:
-            self.discover_cluster_ip_addresses()
-
-        self._cluster_address_map = {}
-        # for cluster_ip_address in self._cluster_ip_addresses:
-        #     _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #     _socket.connect((cluster_ip_address, PORT))
-        #     _socket.sendall(b'GET_ADDRESSES\n')
-        #     data = _socket.recv(1024)
-        #     decoded_data = data.decode('utf-8')
-        #     split_data = decoded_data.split(' ')
-        #     self._cluster_address_map[int(split_data[0])] = {'ip_address': split_data[1]}
-        #     _socket.close()
-        return self._cluster_address_map
